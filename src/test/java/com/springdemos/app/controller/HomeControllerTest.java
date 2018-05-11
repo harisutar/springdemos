112d166af68407bf55e34f0250f7169d4f52d0af
harish <hsutar16@gmail.com>
2018-05-11 20:13:54 +0530

    add controller test classes

---
+++ src/test/java/com/springdemos/app/controller/HomeControllerTest.java
@@ -0,0 +1,21 @@
+package com.springdemos.app.controller;
+
+
+import org.junit.Test;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+public class HomeControllerTest {
+	
+	@Test
+	public void testHomePage() throws Exception {
+		HomeController homeController = new HomeController();
+		
+		MockMvc mockMvc = MockMvcBuilders.standaloneSetup(homeController).build();
+		mockMvc.perform(MockMvcRequestBuilders.get("/"))
+			.andExpect(MockMvcResultMatchers.view().name("home"));		
+		
+	}
+}
